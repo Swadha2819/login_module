512a48331c5d4037979adbfb72601d60f9697929
Swadha2819 <swadha1111@gmail.com>
2025-04-11 12:42:35 +0530

    google-login.php

--- google-login.php
+++ google-login.php
@@ -1,14 +1,15 @@
 <?php
-require_once 'vendor/autoload.php';
 require_once __DIR__ . '/vendor/autoload.php'; 
  // Include Composer autoload
 
 session_start();
 
 $client = new Google_Client();
-$client->setClientId('785784893264-g21f57bq2n99fsjp4naqqo1id8g2pge6.apps.googleusercontent.com');
-$client->setClientSecret('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');
-$client->setRedirectUri('http://localhost:8080/login_module/google_callback.php'); // e.g., http://localhost/google-login.php
+$client->setClientId('785784893264-jak123jkqf2r0pk6cvqtqoirjhu0n0jb.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('http://localhost:8080/login_module/google_callback.php');
+$client->setAccessType('offline');
+$client->setPrompt('consent');
 $client->addScope("email");
 $client->addScope("profile");
 
@@ -16,5 +17,5 @@ $client->addScope("profile");
 $auth_url = $client->createAuthUrl();
 
 // Redirect the user to Google login
-header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+header('Location:'.filter_var($auth_url, FILTER_SANITIZE_URL));
 exit;
